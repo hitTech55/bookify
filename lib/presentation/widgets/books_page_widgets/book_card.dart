f64c033d5abeb41cea454962fc4643c2dbe0989f
maverick <shimpihitesh2@gmail.com>
2023-10-24 23:43:56 +0530

    Added the new page named Book detailed page. Also added gesture detector to the book card

--- lib/presentation/widgets/books_page_widgets/book_card.dart
+++ lib/presentation/widgets/books_page_widgets/book_card.dart
@@ -7,31 +7,36 @@ class BookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: <Widget>[
-            SizedBox(
-              height: 100,
-              width: 120,
-              child: CachedNetworkImage(
-                placeholder: (context,_)=>const CupertinoActivityIndicator(),
-                errorWidget: (context,error,_)=> const Center(child: Icon(Icons.error)),
-                imageUrl: 'https://media.licdn.com/dms/image/D5605AQHeZ-53n9xaWA/feedshare-thumbnail_720_1280/0/1693370817887?e=2147483647&v=beta&t=db5QAA4cYvVPxxfOOFi0xskNP4PGwhuMbP3JowPCUto',
+    return GestureDetector(
+      onTap:(){
+        Navigator.of(context).pushNamed('/bookDetails');
+      },
+      child: Card(
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: <Widget>[
+              SizedBox(
+                height: 100,
+                width: 120,
+                child: CachedNetworkImage(
+                  placeholder: (context,_)=>const CupertinoActivityIndicator(),
+                  errorWidget: (context,error,_)=> const Center(child: Icon(Icons.error)),
+                  imageUrl: 'https://media.licdn.com/dms/image/D5605AQHeZ-53n9xaWA/feedshare-thumbnail_720_1280/0/1693370817887?e=2147483647&v=beta&t=db5QAA4cYvVPxxfOOFi0xskNP4PGwhuMbP3JowPCUto',
+                ),
               ),
-            ),
-            const SizedBox(width: 20),
-            const Column(
-              children: [
-                Text("Name of Book"),
-                Text("Price / Free"),
-                Text("Department / Year"),
-                Text("Publisher Name")
-              ],
-            ),
-          ]
+              const SizedBox(width: 20),
+              const Column(
+                children: [
+                  Text("Name of Book"),
+                  Text("Price / Free"),
+                  Text("Department / Year"),
+                  Text("Publisher Name")
+                ],
+              ),
+            ]
+          ),
         ),
       ),
     );
